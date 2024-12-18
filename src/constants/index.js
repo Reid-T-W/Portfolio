907205458c1c7d924860a93585565f507e8ea710
Reid-T-W <rediettadesse100@gmail.com>
2024-12-18 23:02:27 +0300

    CV link updated

--- src/constants/index.js
+++ src/constants/index.js
@@ -180,7 +180,10 @@ import {
       // source_link: "https://drive.google.com/file/d/1iq4ysVeCdv-SOKITojdZW8hhZbch6k1o/view?usp=sharing",
 
       // Dec 15 2024 Update
-      source_link: "https://drive.google.com/file/d/1KJWrEsxu8xFyN8e3EJng3YFfru2LvLJR/view?usp=sharing"
+      // source_link: "https://drive.google.com/file/d/1KJWrEsxu8xFyN8e3EJng3YFfru2LvLJR/view?usp=sharing"
+
+      // Dec 18 2024
+      source_link: "https://drive.google.com/file/d/1kim2RNynhJWjPCHcQgoXhps3eKGNd20k/view?usp=sharing"
     }
   ];
 
